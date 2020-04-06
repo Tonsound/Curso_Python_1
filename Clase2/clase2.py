6f74c42e7d142cd087eab1aae3551cb2b2765193
unknown <jmmontes@uc.cl>
2020-04-06 18:40:54 -0400

    tercera clase

--- Clase2/clase2.py
+++ Clase2/clase2.py
@@ -32,11 +32,16 @@ division2(6, tres)
 
 division2(6, int(tres))
 
-#atributos: .split() .remove()
+#atributos: .split() .remove() los atributos son características de nuestros elementos que viene prediseñadas
+#  y que podemos utilizar a nuestro gusto, .replace() en una string reemplaza un una parte específica de esta string por otra
+# .split() divide una string en base al argumento entregado
 
 vladimir = "Vladimir Painequeo"
 
 valentin = vladimir.replace("Vladimir", "Valentín")
+
+cinco = 5
+cinco.replace("Vladimir", "Valentín")
 
 lista_valentin = valentin.split(" ")
 
@@ -44,8 +49,17 @@ apellido_valentin = valentin.split(" ")[1]
 
 nombre_valentin = valentin.split(" ")[0]
 
+lista_valentin[0]
 
 #listas
+# Las listas son conjuntos de datos ordenados separados por una coma y encerrados en [], 
+# son un objeto iterable por lo que podemos movernos elemento por elemento. Ojo que parte desde el item 0
+# se pueden tener tambien listas de listas
+# Ej.
+# comida = [["apio", "lechuga", "palta"], ["arroz", "pan", "papas"]]
+# podemos acceder a un elemento de una lista señalando su posición entre brackets,
+# Ej.
+# comida[0] = ["apio", "lechuga", "palta"], comida[0][2] = "palta"
 
 vegetales = ["apio", "lechuga", "palta"]
 
@@ -60,6 +74,10 @@ print(vegetales)
 carbohidratos = ["arroz", "pan", "papas"]
 
 #librerías
+#es un conjunto de elementos (funciones, clases, tipos predefinidos, constantes, variables globales, macros, etc) que es posible utilizar en un programa para facilitar la implementación de ese programa.
+# las llamamos con la string import y podemos cambiarle el nombre con el que la vamos ocupar con la strin "as " + el nombre que queremos usar:
+# Ej.
+# import pandas as pd  --> ahora podemos usar pandas como pd, pd.DataFrame(), si importamos asi: importa pandas  --> debemos usar el nombre completo pandas.DataFrame()
 import random
 
 print(random.choice(vegetales))
@@ -70,29 +88,26 @@ help(random)
 def Sugerencia_comida(carbohidratos, vegetales):
     print(random.choice(carbohidratos) + " con " + random.choice(vegetales))
 
+
 #loop for
+# nos permite iterar sobre una secuencia por cada iteración podemos ejecutar un código, podemos anidar cuantos loops queramos,
+# pero en general existen otras fórmulas mejores para hacer iteraciones sobre iteraciones
+
 def todos_los_carbohidratos(carbohidratos):
+    elementos = []
     for element in carbohidratos:
-        print(element)
+        elementos.append(element)
+    return elementos
+
+carb = todos_los_carbohidratos(carbohidratos)
 
 def menus_posibles(carbohidratos, vegetales):
-    for element in carbohidratos:
-        for elemento in vegetales:
-            print(element + " con " + elemento)   
+    for a in carbohidratos:
+        for b in vegetales:
+            print(a + " con " + b)   
 
-
-
-#Diccionarios
-persona1 = {"edad":10, "nombre": "Marcos", "poder": 5}
-persona2 = {"edad":12 ,"nombre": "Jaime", "poder": 7}
-
-#If
-
-
-#Loop while
-
-
-#time.sleep() datetime()
+for i in range(len(carbohidratos)):
+    print(carbohidratos[i])
 
 
 
---
+++ Clase2/tarea2.py
@@ -0,0 +1,10 @@
+# Cree un loop que imprima la sucesión de fibonacci F(n+1) --> 0,1,1,2,3,5,8,13,21,34...
+# Tenga cuidado de que no entre en un loop eterno, para ello puede iterar en algun rango, ej. for i in range(20):
+
+
+# Importe la bliblioteca time y use la función time.sleep(2) en su ejercicio anterior para poner una pausa en cada iteración de fibonacci
+# Puede cambiar el argumento "2" dentro de time.sleep para variar la duración de la pausa
+
+# Cree una función que haga una nueva lista con todos los menus posibles vistos en la clase anterior
+
+
