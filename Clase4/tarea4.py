2fa316f0b1f01e6c15602485223012129130bdc6
unknown <jmmontes@uc.cl>
2020-04-26 14:22:37 -0400

    tarea2

--- Clase4/tarea4.py
+++ Clase4/tarea4.py
@@ -7,6 +7,8 @@ df = pandas.DataFrame(frame_data, index = ['James', 'Jason', 'Rogers'])
 df2 = df.drop(['Rogers']) #elimina la fila en que se encuentre 'Rogers' dentro de la columna indice, en esta base de datos se establecio que el index es la cokumna con los nombres, si no agregamos un index, pandas elaborará uno numerico y lo agregará como columna index
 df2 = df.drop(df.index[[0, 1]]) #elimina un rango de filas
 
+df2 = df.tanspose() #transpone la base de datos
+
 suma= df['A'].sum() # esto nos permite sumar los datos de la columna 'A'
 valores_unicos =  df['A'].nunique() #cuenta los valores unicos
 cantidad de datos = df['A'].count() # cuenta todos los valores de la columna 'A'
