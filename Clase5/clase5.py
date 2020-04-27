a9e10b417c4885300ba9fabdc71ecded94f2d9ef
unknown <jmmontes@uc.cl>
2020-04-26 20:31:48 -0400

    tarea2

--- Clase5/clase5.py
+++ Clase5/clase5.py
@@ -8,19 +8,21 @@ df = pd.DataFrame(lista)
 df2 = df.transpose() #transpone la base de datos
 
 frame_data = {'name': ['James', 'Jason', 'Rogers'], 'age': [18, 20, 22], 'job': ['Assistant', 'Manager', 'Clerk']}
-df = pd.DataFrame(frame_data, index = ['James', 'Jason', 'Rogers'])
+df = pd.DataFrame(frame_data, index = frame_data['name'])
 
 df2 = df.drop(['Rogers']) #elimina la fila en que se encuentre 'Rogers' dentro de la columna indice, en esta base de datos se establecio que el index es la cokumna con los nombres, si no agregamos un index, pandas elaborará uno numerico y lo agregará como columna index
-df2 = df.drop(df.index[[0, 1]]) #elimina un rango de filas
+df2 = df.drop(df.index[[1, 1]]) #elimina un rango de filas
 
 df2 = df.drop_duplicates(['name']) #bota los duplicados de la columna name
 df2 = df.drop(['job'], axis=1) #elimina la columna 'job' Si el valor del eje es 1 significa que queremos eliminar columnas, si el valor del eje es 0 o se deja en blanco significa que la fila se eliminará
 
 
-suma= df['age'].sum() # esto nos permite sumar los datos de la columna 'A'
+suma = df['age'].sum() # esto nos permite sumar los datos de la columna 'A'
 valores_unicos =  df['age'].nunique() #cuenta los valores unicos
-cantidad de datos = df['age'].count() # cuenta todos los valores de la columna 'A'
-df[0:3] #selecciona las 3 primeras columnas
+cantidad_de_datos = df['age'].count() # cuenta todos los valores de la columna 'A'
+df[0:1] #selecciona las 3 primeras filas
+
+df2 = df[df['Estado interno'] == 'problemas']
 
 
 
@@ -71,6 +73,34 @@ def get_citas(lista):
     for i in range(len(pags)):
         if '[*]' in pags[i]:
             citas.append(pags[i].split('[*]')[1])
-            print(citas)
+    print('Listas tomadas')
 
 get_citas(pags)
+
+autores = []
+
+def get_autores(lista):
+    for i in range(len(pags)):
+        if '[*]' in pags[i]:
+            cita = pags[i].split('[*]')[1]
+            autor = cita.split('\n')[0]
+            autores.append(autor)
+    print('Listas tomadas')
+
+
+get_autores(pags)
+
+from pandas import ExcelWriter as ew
+import pandas as pd
+
+df = pd.DataFrame(autores)
+
+def generar_DF_Excel(df, nombre_archivo):
+    nombre_final = nombre_archivo + '.xlsx' 
+    writer = ew(nombre_final)
+    df.to_excel(writer,'Orders2')
+    writer.save()
+    print('Ok')
+
+
+generar_DF_Excel(df, 'autores sopa de wuhan')
